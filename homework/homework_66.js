f8a5fc02695f3233fb6b4a9b70fb27ec3c257501
Alibek <sultanovalibek2020@gmail.com>
2026-05-14 16:54:57 +0600

    Задание 1:вывести строки скороговорки в консоль в правильном порядке(использовали getElementById, innerHTML, console.log)

---
+++ homework/homework_66.js
@@ -0,0 +1,19 @@
+'use strict';
+
+window.onload = function () {
+
+    const string3 = document.getElementById('string-3');
+    const string5 = document.getElementById('string-5');
+    const string2 = document.getElementById('string-2');
+    const string6 = document.getElementById('string-6');
+    const string4 = document.getElementById('string-4');
+    const string1 = document.getElementById('string-1');
+
+    console.log(string3.innerHTML);
+    console.log(string5.innerHTML);
+    console.log(string2.innerHTML);
+    console.log(string6.innerHTML);
+    console.log(string4.innerHTML);
+    console.log(string1.innerHTML);
+
+};
